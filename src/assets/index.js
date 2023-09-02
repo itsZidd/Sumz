1d93b68d605566082cb475e7e80bf56d85c36f77
ZiddanMalik <zidanmalik02@gmail.com>
2023-09-02 19:11:44 +0700

    final

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import logo from './logo.svg'
+import sendIcon from './send.svg'
+import tick from './tick.svg'
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    logo,
+    sendIcon,
+    tick
+}
